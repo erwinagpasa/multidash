dadf739aaf285384e694fb22ea89323e8e304b5b
erwinagpasa <istran.net@gmail.com>
2021-12-15 10:46:35 +0300

    installed bootstrap,jquery, and popper

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "jquery/src/jquery"; //for bootstrap.min.js
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 function App() {
   return (
@@ -17,6 +20,35 @@ function App() {
         >
           Learn React
         </a>
+
+        <button type="button" className="btn btn-primary">
+          Primary
+        </button>
+        <button type="button" className="btn btn-secondary">
+          Secondary
+        </button>
+        <button type="button" className="btn btn-success">
+          Success
+        </button>
+        <button type="button" className="btn btn-danger">
+          Danger
+        </button>
+        <button type="button" className="btn btn-warning">
+          Warning
+        </button>
+        <button type="button" className="btn btn-info">
+          Info
+        </button>
+        <button type="button" className="btn btn-light">
+          Light
+        </button>
+        <button type="button" className="btn btn-dark">
+          Dark
+        </button>
+
+        <button type="button" className="btn btn-link">
+          Link
+        </button>
       </header>
     </div>
   );
